7f6f5bdfddad20a1bb124b082cbfa1bcd7277542
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 14:22:35 -0300

    create route of account debit

---
+++ src/components/pages/AccountsDebit/index.tsx
@@ -0,0 +1,10 @@
+import ListAccounts from '@/components/ListAccounts'
+import * as S from './styles'
+
+const AccountsDebit = () => (
+  <S.Wrapper>
+    <ListAccounts />
+  </S.Wrapper>
+)
+
+export default AccountsDebit
---
+++ src/pages/accounts-debit.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import AccountDebit from '@/components/pages/AccountsDebit'
+
+export default function AccountsDebit() {
+  return (
+    <DefaultLayout>
+      <AccountDebit />
+    </DefaultLayout>
+  )
+}
