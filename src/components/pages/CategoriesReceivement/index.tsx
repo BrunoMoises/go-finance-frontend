806e34e77ca2f83381a255a7badd078584006468
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 18:44:55 -0300

    create page of categories receivement

---
+++ src/components/pages/CategoriesReceivement/index.tsx
@@ -0,0 +1,10 @@
+import ListCategories from '@/components/ListCategories'
+import * as S from './styles'
+
+const CategoriesReceivement = () => (
+  <S.Wrapper>
+    <ListCategories />
+  </S.Wrapper>
+)
+
+export default CategoriesReceivement
