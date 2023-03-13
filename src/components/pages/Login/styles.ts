4dd8a2f7d4173574266fed77c2f56fc3e39be1f6
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 09:28:01 -0300

    create login page

---
+++ src/components/pages/Login/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    height: 60vh;
+    width: 100%;
+
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const ImgLogin = styled.img`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    width: 200px;
+    margin-bottom: 20px;
+`
