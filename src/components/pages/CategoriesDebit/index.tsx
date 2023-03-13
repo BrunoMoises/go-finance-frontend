944acf44b0edb8cc22facbdb039ec6d4501c5d90
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 18:44:06 -0300

    create page of categories debit

---
+++ src/components/pages/CategoriesDebit/index.tsx
@@ -0,0 +1,10 @@
+import ListCategories from '@/components/ListCategories'
+import * as S from './styles'
+
+const CategoriesDebit = () => (
+  <S.Wrapper>
+    <ListCategories />
+  </S.Wrapper>
+)
+
+export default CategoriesDebit
