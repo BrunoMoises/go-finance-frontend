0b661a362c5b6b5fe6aa371089a1af303aa6e619
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 18:43:16 -0300

    create page of accounts receivement

---
+++ src/components/pages/AccountsReceivement/index.tsx
@@ -0,0 +1,10 @@
+import ListAccounts from '@/components/ListAccounts'
+import * as S from './styles'
+
+const AccountsReceivement = () => (
+  <S.Wrapper>
+    <ListAccounts />
+  </S.Wrapper>
+)
+
+export default AccountsReceivement
