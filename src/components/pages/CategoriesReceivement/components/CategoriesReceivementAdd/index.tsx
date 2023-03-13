b2cc245dbd662cd63eb0e07ab6375954c45bb906
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 19:08:28 -0300

    create categories receivement add

---
+++ src/components/pages/CategoriesReceivement/components/CategoriesReceivementAdd/index.tsx
@@ -0,0 +1,19 @@
+import Modal from '@/components/Modal'
+import CategoriesReceivementForm from '../CategoriesReceivementForm'
+
+export type CategoriesReceivementAddProps = {
+    isOpen: boolean
+    getValueOpen: (value: boolean) => void
+}
+
+const CategoriesReceivementAdd = ({ isOpen, getValueOpen }: CategoriesReceivementAddProps) => (
+    <Modal
+        title="Adicionar novo recebimento"
+        isOpen={isOpen}
+        getValueOpen={getValueOpen}
+    >
+        <CategoriesReceivementForm />
+    </Modal>
+)
+
+export default CategoriesReceivementAdd
