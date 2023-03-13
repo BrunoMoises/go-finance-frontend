1ed4b726fabe3bf615a15cd9ca4ffd47d018779a
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 20:09:33 -0300

    add onclick in button 'Adicionar' of categories receivement

--- src/components/pages/CategoriesReceivement/index.tsx
+++ src/components/pages/CategoriesReceivement/index.tsx
@@ -1,10 +1,19 @@
 import ListCategories from '@/components/ListCategories'
+import { useState } from 'react'
+import CategoriesReceivementAdd from './components/CategoriesReceivementAdd'
 import * as S from './styles'
 
-const CategoriesReceivement = () => (
-  <S.Wrapper>
-    <ListCategories />
-  </S.Wrapper>
-)
+const CategoriesReceivement = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const openAddModal = (value: boolean) => {
+    setModalIsOpen(true)
+  }
+  return (
+    <S.Wrapper>
+      <CategoriesReceivementAdd getValueOpen={openAddModal} isOpen={modalIsOpen} />
+      <ListCategories openModal={openAddModal} />
+    </S.Wrapper>
+  )
+}
 
 export default CategoriesReceivement
