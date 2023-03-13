e8bf7f1c727133b541f34156dbcdcd5a329168bf
Bruno <bruno_moises_stz@hotmail.com>
2023-03-13 19:08:37 -0300

    create categories receivement form

---
+++ src/components/pages/CategoriesReceivement/components/CategoriesReceivementForm/index.tsx
@@ -0,0 +1,17 @@
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import * as S from './styles'
+
+export default function CategoriesReceivementForm() {
+    return (
+        <S.Container>
+            <S.Inputs>
+                <Input name='title' label='Titulo' placeholder='Digite o titulo da categoria...' />
+                <Input name='description' label='Descrição' placeholder='Digite a descrição...' />
+            </S.Inputs>
+            <Button>
+                Salvar
+            </Button>
+        </S.Container>
+    )
+}
