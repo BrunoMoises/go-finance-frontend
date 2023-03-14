a5575849c3c8178a3dcdf77a057d324ec1ab39ce
Bruno <bruno_moises_stz@hotmail.com>
2023-03-14 14:01:13 -0300

    create modal of accounts debit

---
+++ src/components/pages/AccountsDebit/components/AccountsDebitForm/index.tsx
@@ -0,0 +1,22 @@
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import * as S from './styles'
+
+export default function AccountsDebitForm() {
+    return (
+        <S.Container>
+            <S.Inputs>
+                <Input name='title' label='Titulo' placeholder='Titulo da categoria...' />
+                <Input name='description' label='Descrição' placeholder='Digite a descrição...' />
+                <Input name='category' label='Categoria' placeholder='Selecione...' />
+                <Input name='value' label='Valor' placeholder='Digite o valor...' type='number' />
+                <Input name='date' label='Data' placeholder='Digite a data...' type='date' />
+            </S.Inputs>
+            <S.Buttons>
+                <Button>
+                    Salvar
+                </Button>
+            </S.Buttons>
+        </S.Container>
+    )
+}
